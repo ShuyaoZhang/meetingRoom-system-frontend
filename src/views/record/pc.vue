b724612e7227d7f8d6373431bed83bd47ef95ae2
ShuyaoZhang <jjiahuang@xlcwnet.com>
2021-02-26 17:27:04 +0800

    提交预定记录的代码

---
+++ src/views/record/pc.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pcRecord">
+        <el-tabs type="border-card">
+            <el-tab-pane>
+                <span slot="label"><i class="el-icon-success"></i> 已通过</span>
+                我的行程
+            </el-tab-pane>
+            <el-tab-pane>
+                <span slot="label"><i class="el-icon-error"></i> 已驳回</span>
+                我的行程
+            </el-tab-pane>
+            <el-tab-pane>
+                <span slot="label"><i class="el-icon-loading"></i> 等待审批</span>
+                我的行程
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        mounted() {},
+        methods: {},
+    }
+</script>
+<style lang="scss" scoped>
+
+</style>
